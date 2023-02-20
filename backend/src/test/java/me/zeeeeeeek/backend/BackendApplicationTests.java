71075df7bcef1b1036076fa358f38bd5dec62971
Enrico Ulissi <enricoulissi01@gmail.com>
2023-02-20 17:18:09 +0100

    Initial commit

---
+++ backend/src/test/java/me/zeeeeeeek/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeeeeeeek.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
