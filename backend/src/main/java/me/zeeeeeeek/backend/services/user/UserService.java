aca6ea1826bdadb82715a950a4c34ae49fd96e16
Enrico Ulissi <enricoulissi01@gmail.com>
2023-02-23 16:16:42 +0100

    Implemented a fake user service.

---
+++ backend/src/main/java/me/zeeeeeeek/backend/services/user/UserService.java
@@ -0,0 +1,28 @@
+package me.zeeeeeeek.backend.services.user;
+
+import me.zeeeeeeek.backend.models.user.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Spring service that handles the business logic for the user.
+ */
+@Service
+public class UserService {
+
+    /**
+     * Gets a user by its UUID.
+     *
+     * @param uuid the UUID of the user
+     * @return the user
+     * @throws IllegalArgumentException if the user is not found
+     * @throws NullPointerException     if the UUID is null
+     */
+    public User getById(UUID uuid) {
+        Objects.requireNonNull(uuid, "The UUID cannot be null");
+        //Todo implement a user retrieval from the database using the UUID
+        return new User("aaa", "bbb", "css@ca", "ddd", "eee");
+    }
+}
