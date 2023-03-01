827bd9e25e4e4ea1afb6ec9015334d7c749073c9
Enrico Ulissi <enricoulissi01@gmail.com>
2023-03-01 13:25:58 +0100

    add sql injection prevention

--- backend/src/main/java/me/zeeeeeeek/backend/repositories/AbstractTaskRepository.java
+++ backend/src/main/java/me/zeeeeeeek/backend/repositories/AbstractTaskRepository.java
@@ -1,8 +1,10 @@
 package me.zeeeeeeek.backend.repositories;
 
+import lombok.NonNull;
 import me.zeeeeeeek.backend.models.tasks.elements.AbstractTask;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
@@ -10,7 +12,7 @@ public interface AbstractTaskRepository extends CrudRepository<AbstractTask, UUI
     /**
      * Find all the tasks of a give TaskList.
      */
-    @Query("SELECT t FROM AbstractTask t WHERE t.taskList.id = ?1")
-    Iterable<AbstractTask> findAllByTaskListId(UUID taskListId);
+    @Query("SELECT t FROM AbstractTask t WHERE t.taskList.id = :taskListId")
+    Iterable<AbstractTask> findAllByTaskListId(@Param("taskListId") @NonNull UUID taskListId);
 
 }
--- backend/src/main/java/me/zeeeeeeek/backend/repositories/TaskListRepository.java
+++ backend/src/main/java/me/zeeeeeeek/backend/repositories/TaskListRepository.java
@@ -1,17 +1,19 @@
 package me.zeeeeeeek.backend.repositories;
 
+import lombok.NonNull;
 import me.zeeeeeeek.backend.models.tasks.collections.TaskList;
 import me.zeeeeeeek.backend.models.user.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TaskListRepository extends CrudRepository<TaskList, UUID> {
 
-    @Query("SELECT tl FROM taskLists tl WHERE tl.owner = ?1")
-    List<TaskList> findAllOwnedBy(User owner);
+    @Query("SELECT tl FROM taskLists tl WHERE tl.owner = :user")
+    List<TaskList> findAllOwnedBy(@Param("user") @NonNull User owner);
 
-    TaskList findByOwner(User owner);
+    TaskList findByOwner(@Param("owner") @NonNull User owner);
 }
