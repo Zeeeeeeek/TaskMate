49ff2a590c25066cc449167f78176f3d754db2d5
Enrico Ulissi <enricoulissi01@gmail.com>
2023-03-02 17:39:56 +0100

    remove repeated methods and UserServiceTest

--- backend/src/main/java/me/zeeeeeeek/backend/services/user/UserService.java
+++ backend/src/main/java/me/zeeeeeeek/backend/services/user/UserService.java
@@ -1,11 +1,8 @@
 package me.zeeeeeeek.backend.services.user;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.zeeeeeeek.backend.models.user.Role;
 import me.zeeeeeeek.backend.models.user.User;
-import me.zeeeeeeek.backend.models.user.dtos.UserCreationDTO;
-import me.zeeeeeeek.backend.models.user.dtos.UserDTO;
-import me.zeeeeeeek.backend.models.user.dtos.UserDTOMapper;
 import me.zeeeeeeek.backend.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,32 +19,10 @@ import java.util.UUID;
  */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    private final UserDTOMapper userDtoMapper;
-
-    public UserService(UserRepository userRepository, UserDTOMapper userDtoMapper) {
-        this.userRepository = userRepository;
-        this.userDtoMapper = userDtoMapper;
-    }
-
-
-    /**
-     * Creates and saves a new user from the given data transfer object.
-     * This operation is equivalent to calling {@link #create(UserCreationDTO)} and {@link #save(User)}.
-     * <p>
-     * This method is useful when you want to create a user and save it in the database in a single operation.
-     * </p>
-     *
-     * @param userCreationDTO the data transfer object for the user creation
-     * @return the created user
-     */
-    public UserDTO createAndSave(UserCreationDTO userCreationDTO) {
-        User user = this.create(userCreationDTO);
-        log.info("Created user {}", user);
-        return this.save(user);
-    }
 
 
     public List<User> getAll() {
@@ -68,48 +43,6 @@ public class UserService implements UserDetailsService {
         return this.userRepository
                 .findById(Objects.requireNonNull(uuid))
                 .orElseThrow(() -> new IllegalArgumentException("User with UUID " + uuid + " does not exist"));
-    }
-
-    /**
-     * Saves a user in the storage system.
-     *
-     * @param user the user to save
-     * @return the saved user as a data transfer object.
-     * @throws NullPointerException if the user is null
-     */
-    public UserDTO save(User user) {
-        this.userRepository.save(Objects.requireNonNull(user));
-        return this.userToUserDto(user);
-    }
-
-    /**
-     * Creates a new user from the given data transfer object.
-     *
-     * @param userCreationDTO the data transfer object for the user creation
-     * @return the created user
-     * @throws NullPointerException if the data transfer object is null
-     */
-
-    public User create(UserCreationDTO userCreationDTO) {
-        Objects.requireNonNull(userCreationDTO);
-        return new User(userCreationDTO.username(),
-                userCreationDTO.password(),
-                userCreationDTO.email(),
-                userCreationDTO.firstName(),
-                userCreationDTO.lastName(),
-                Role.USER);
-    }
-
-    /**
-     * Gets a UserDTO from a User instance.
-     *
-     * @param user the user
-     * @return the user as a data transfer object
-     * @throws NullPointerException if the user is null
-     */
-    public UserDTO userToUserDto(User user) {
-        return this.userDtoMapper
-                .mapToDTO(Objects.requireNonNull(user));
     }
 
     /**
