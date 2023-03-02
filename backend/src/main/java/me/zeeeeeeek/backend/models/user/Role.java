ec158af80db78c9177e378eff1fee0c8f56fa6b4
Enrico Ulissi <enricoulissi01@gmail.com>
2023-03-02 11:50:45 +0100

    implement UserDetails and add Roles

---
+++ backend/src/main/java/me/zeeeeeeek/backend/models/user/Role.java
@@ -0,0 +1,5 @@
+package me.zeeeeeeek.backend.models.user;
+
+public enum Role {
+    USER
+}
--- backend/src/main/java/me/zeeeeeeek/backend/models/user/User.java
+++ backend/src/main/java/me/zeeeeeeek/backend/models/user/User.java
@@ -7,8 +7,12 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import me.zeeeeeeek.backend.models.tasks.collections.TaskList;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,7 +24,7 @@ import java.util.UUID;
 @Data
 @Slf4j
 @Entity(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -37,6 +41,9 @@ public class User {
 
     @OneToMany(mappedBy = "owner")
     private List<TaskList> taskLists;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     /**
      * Create a new user.
@@ -77,4 +84,61 @@ public class User {
         return email;
     }
 
+    /**
+     * Returns the authorities granted to the user. Cannot return <code>null</code>.
+     *
+     * @return the authorities, sorted by natural key (never <code>null</code>)
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority(role.name())
+        );
+    }
+
+    /**
+     * Indicates whether the user's account has expired. An expired account cannot be
+     * authenticated.
+     *
+     * @return <code>true</code> if the user's account is valid (ie non-expired),
+     * <code>false</code> if no longer valid (ie expired)
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * Indicates whether the user is locked or unlocked. A locked user cannot be
+     * authenticated.
+     *
+     * @return <code>true</code> if the user is not locked, <code>false</code> otherwise
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * Indicates whether the user's credentials (password) has expired. Expired
+     * credentials prevent authentication.
+     *
+     * @return <code>true</code> if the user's credentials are valid (ie non-expired),
+     * <code>false</code> if no longer valid (ie expired)
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * Indicates whether the user is enabled or disabled. A disabled user cannot be
+     * authenticated.
+     *
+     * @return <code>true</code> if the user is enabled, <code>false</code> otherwise
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
