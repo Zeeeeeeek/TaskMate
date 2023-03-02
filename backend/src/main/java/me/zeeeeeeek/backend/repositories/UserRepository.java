66432312d878c810ca2ab5fbee1bc3781a0a2a77
Enrico Ulissi <enricoulissi01@gmail.com>
2023-03-02 11:53:48 +0100

    add user find by email and username

--- backend/src/main/java/me/zeeeeeeek/backend/repositories/UserRepository.java
+++ backend/src/main/java/me/zeeeeeeek/backend/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package me.zeeeeeeek.backend.repositories;
 
+import lombok.NonNull;
 import me.zeeeeeeek.backend.models.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
+
+    Optional<User> findByUsername(@NonNull String username);
+
+    Optional<User> findByEmail(@NonNull String email);
 }
