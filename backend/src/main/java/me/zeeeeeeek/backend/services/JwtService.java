b979e8e5b861eeed431f943e10ffa5153783ff8b
Enrico Ulissi <enricoulissi01@gmail.com>
2023-03-02 12:27:11 +0100

    add jwt filter and service

---
+++ backend/src/main/java/me/zeeeeeeek/backend/services/JwtService.java
@@ -0,0 +1,11 @@
+package me.zeeeeeeek.backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String jwtToken) {
+        return "";
+    }
+}
