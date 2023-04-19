b314d82f7c84bca3410cc001a49fcac0da07c954
Enrico Ulissi <enricoulissi01@gmail.com>
2023-04-19 22:46:59 +0200

    remove init user

--- backend/src/main/java/me/zeeeeeeek/backend/BackendApplication.java
+++ backend/src/main/java/me/zeeeeeeek/backend/BackendApplication.java
@@ -1,12 +1,8 @@
 package me.zeeeeeeek.backend;
 
 import lombok.extern.slf4j.Slf4j;
-import me.zeeeeeeek.backend.models.user.dtos.UserCreationDTO;
-import me.zeeeeeeek.backend.services.auth.AuthService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
@@ -16,19 +12,4 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
         log.info("Application is running on http://localhost:8080");
     }
-
-    @Bean
-    public CommandLineRunner initUser(AuthService authService) {
-        return (args) -> {
-            authService.register(
-                    new UserCreationDTO(
-                            "a",
-                            "a",
-                            "admin@admin"
-                    )
-            );
-        };
-    }
-
-
 }
