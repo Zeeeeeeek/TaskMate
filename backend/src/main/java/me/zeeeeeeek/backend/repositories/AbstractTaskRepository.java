18f05cb1a04e0d0fae0581213d3c6c5c52b42f97
enrico <enricoulissi01@gmail.com>
2023-05-20 14:07:07 +0200

    remove unused method

--- backend/src/main/java/me/zeeeeeeek/backend/repositories/AbstractTaskRepository.java
+++ backend/src/main/java/me/zeeeeeeek/backend/repositories/AbstractTaskRepository.java
@@ -1,18 +1,10 @@
 package me.zeeeeeeek.backend.repositories;
 
-import lombok.NonNull;
 import me.zeeeeeeek.backend.models.tasks.elements.AbstractTask;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface AbstractTaskRepository extends CrudRepository<AbstractTask, UUID> {
-    /**
-     * Find all the tasks of a give TaskList.
-     */
-    @Query("SELECT t FROM AbstractTask t WHERE t.taskList.id = :taskListId")
-    Iterable<AbstractTask> findAllByTaskListId(@Param("taskListId") @NonNull UUID taskListId);
 
 }
