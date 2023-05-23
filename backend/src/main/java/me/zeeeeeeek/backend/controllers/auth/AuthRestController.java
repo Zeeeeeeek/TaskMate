849e379a671efb8e53565cc7a3cfc5242a37743a
enrico <enricoulissi01@gmail.com>
2023-05-23 19:13:50 +0200

    single imports

--- backend/src/main/java/me/zeeeeeeek/backend/controllers/auth/AuthRestController.java
+++ backend/src/main/java/me/zeeeeeeek/backend/controllers/auth/AuthRestController.java
@@ -6,7 +6,13 @@ import me.zeeeeeeek.backend.models.user.dtos.UserCreationDTO;
 import me.zeeeeeeek.backend.models.user.dtos.UserLoginDto;
 import me.zeeeeeeek.backend.services.auth.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
