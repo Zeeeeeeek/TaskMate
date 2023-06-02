4b7200d89176e1e2490aa0601d1268be27eb17cb
Enrico Ulissi <enricoulissi01@gmail.com>
2023-06-02 11:36:24 +0200

    weak password exception

--- backend/src/main/java/me/zeeeeeeek/backend/exceptions/handlers/UserExceptionHandler.java
+++ backend/src/main/java/me/zeeeeeeek/backend/exceptions/handlers/UserExceptionHandler.java
@@ -1,12 +1,14 @@
 package me.zeeeeeeek.backend.exceptions.handlers;
 
 import me.zeeeeeeek.backend.exceptions.users.CredentialsAlreadyUsed;
+import me.zeeeeeeek.backend.exceptions.users.WeakPasswordException;
 import me.zeeeeeeek.backend.utils.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @ControllerAdvice
 public class UserExceptionHandler {
@@ -15,6 +17,12 @@ public class UserExceptionHandler {
     public ResponseEntity<ErrorResponse> handleCredentialsAlreadyUsed(CredentialsAlreadyUsed e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
         return new ResponseEntity<>(errorResponse, CONFLICT);
+    }
+
+    @ExceptionHandler(WeakPasswordException.class)
+    public ResponseEntity<ErrorResponse> handleWeakPasswordException(WeakPasswordException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, UNPROCESSABLE_ENTITY);
     }
 
 }
